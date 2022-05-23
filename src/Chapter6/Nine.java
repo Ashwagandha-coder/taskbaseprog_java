b2f2c8cd4548dc6e165e9dfb444d1346858f3015
Ashwagandha-coder <max.cacz@yandex.ru>
2022-05-23 20:44:55 +0500

    ready task 6.9

--- src/Chapter6/Nine.java
+++ src/Chapter6/Nine.java
@@ -1,4 +1,47 @@
 package Chapter6;
 
+import java.io.PrintStream;
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
+
 public class Nine {
+
+
+    public static void main(String[] args) {
+
+        new Checked6();
+
+    }
+}
+
+final class Checked6 {
+
+    private int CHISLO;
+    private final Check check = () -> {
+
+        if (CHISLO % 10 == CHISLO / 100 && (CHISLO % 100) / 10 == CHISLO % 10)
+            new PrintStream(System.out,true, StandardCharsets.UTF_8).println("Четно-красивое");
+        else
+            System.out.println("Not");
+
+    };
+
+    public Checked6() {
+
+        Scanner scanner = new Scanner(System.in);
+        this.CHISLO = scanner.nextInt();
+
+        check.CheckedChislo6();
+    }
+
+
+
+
+}
+
+@FunctionalInterface
+interface Check {
+
+    void CheckedChislo6();
+
 }
